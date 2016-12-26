e348674f7d8683b18375cb54c95f34fa8f9dee07
Evan Davis <me@ezdwt.com>
2016-12-26 15:51:16 -0800

    Factored common check into helper method in functional test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -16,6 +16,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Evan has heard about a cool new online to-do app. He goes to check out
         # its homepage.
@@ -40,10 +45,7 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter, the page updates, and now the page lists "1: Buy
         # peacock feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting him to add another item. He enters
         # "Use peacock feathers to make a fly" (Evan is very methodical)
@@ -52,13 +54,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Evan wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some

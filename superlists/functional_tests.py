2d10040c11ce3402b5d73a7bdff841c1968e9bd1
Evan Davis <me@ezdwt.com>
2016-12-26 12:38:22 -0800

    First FT specced out in comments, and now uses unittest.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,11 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
 
-browser = webdriver.Firefox(
-    firefox_binary=FirefoxBinary(
-        firefox_path=r"C:\Program Files (x86)\Mozilla Firefox ESR\firefox.exe"
-    )
-)
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox(
+            firefox_binary=FirefoxBinary(
+            firefox_path=r"C:\Program Files (x86)\Mozilla Firefox ESR\firefox.exe"
+            )
+        )
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Evan has heard about a cool new online to-do app. He goes to check out
+        # its homepage.
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # He is invited to enter a to-do item straight away
+
+        # He types "Buy peacock feathers" into a text box (Evan's hobby is tying
+        # fly-fishing lures)
+
+        # When he hits enter, the page updates, and now the page lists "1: Buy
+        # peacock feathers" as an item in a to-do list.
+
+        # There is still a text box inviting him to add another item. He enters
+        # "Use peacock feathers to make a fly" (Evan is very methodical)
+
+        # The page updates again, and now shows both items on his list
+
+        # Evan wonders whether the site will remember his list. Then he sees
+        # that the site has generated a unique URL for him -- there is some
+        # explanatory text to that effect.
+
+        # He visits that URL -- his to-do list is still there.
+
+        # Satisfied, he goes back to sleep.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')

4cd96b4a82e590544943e543597787d8b1060bde
Evan Davis <me@ezdwt.com>
2016-12-27 09:13:42 -0800

    Added functional test for multiple lists functionality

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -6,6 +6,15 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
+        self.browser = None
+        self.new_browser_session()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def new_browser_session(self):
+        if self.browser:
+            self.browser.quit()
         self.browser = webdriver.Firefox(
             firefox_binary=FirefoxBinary(
                 firefox_path=r"C:\Program Files (x86)\Mozilla Firefox ESR\firefox.exe"
@@ -13,15 +22,12 @@ class NewVisitorTest(LiveServerTestCase):
         )
         self.browser.implicitly_wait(3)
 
-    def tearDown(self):
-        self.browser.quit()
-
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Evan has heard about a cool new online to-do app. He goes to check out
         # its homepage.
         self.browser.get(self.live_server_url)
@@ -57,11 +63,49 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Evan wonders whether the site will remember his list. Then he sees
-        # that the site has generated a unique URL for him -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
-
-        # He visits that URL -- his to-do list is still there.
-
         # Satisfied, he goes back to sleep.
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Evan starts a new todo list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # He notices that his list has a unique URL
+        evan_list_url = self.browser.current_url
+        self.assertRegex(evan_list_url, '/lists/.+')
+
+        # Now a new user, Calvin, comes along to the site
+
+        ## We use a new browser session to make sure that no
+        ## information of Evan's is coming through from cookies etc
+        self.new_browser_session()
+
+        # Calvin visits the home page. There is no sign of Evan's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Calvin starts a new list by entering a new item. He is
+        # less interesting than Evan...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        # Calvin gets his own unique URL
+        calvin_list_url = self.browser.current_url
+        self.assertRegex(calvin_list_url, '/lists/.+')
+        self.assertNotEqual(calvin_list_url, evan_list_url)
+
+        # Again, there is no trace of Evan's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
+

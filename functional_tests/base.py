99dc77e06bfdc5eaba43bca264d5e2a4690a30a4
Evan Davis <me@ezdwt.com>
2016-12-30 14:45:13 -0800

    Moved FTs into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,46 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import sys
+from unittest import skip
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
+    def setUp(self):
+        self.browser = None
+        self.new_browser_session()
+
+    def tearDown(self):
+        self.browser.refresh()
+        self.browser.quit()
+
+    def new_browser_session(self):
+        if self.browser:
+            self.browser.refresh()
+            self.browser.quit()
+        self.browser = webdriver.Firefox(
+            firefox_binary=FirefoxBinary(
+                firefox_path=r"C:\Program Files (x86)\Mozilla Firefox ESR\firefox.exe"
+            )
+        )
+        self.browser.implicitly_wait(3)
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,49 +1,8 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
+from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import sys
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-
-    def setUp(self):
-        self.browser = None
-        self.new_browser_session()
-
-    def tearDown(self):
-        self.browser.refresh()
-        self.browser.quit()
-
-    def new_browser_session(self):
-        if self.browser:
-            self.browser.refresh()
-            self.browser.quit()
-        self.browser = webdriver.Firefox(
-            firefox_binary=FirefoxBinary(
-                firefox_path=r"C:\Program Files (x86)\Mozilla Firefox ESR\firefox.exe"
-            )
-        )
-        self.browser.implicitly_wait(3)
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_one_user(self):
         # Evan has heard about a cool new online to-do app. He goes to check out
         # its homepage.
@@ -125,26 +84,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
-    def test_layout_and_styling(self):
-        # Evan goes to the home page
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # He notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/2,
-            512,
-            delta=10
-        )
-
-        # He starts a new list and sees the input is nicely centered
-        # there too
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/2,
-            512,
-            delta=10
-        )

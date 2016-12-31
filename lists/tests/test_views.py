14e7828027e4146479a794a889ce52b54404386c
Evan Davis <me@ezdwt.com>
2016-12-31 13:55:44 -0800

    Create new form for adding an item to an existing list w/ duplicate validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.utils.html import escape
-from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR, ItemForm
 from lists.models import Item, List
+from unittest import skip
 
 
 class HomePageTest(TestCase):
@@ -89,6 +90,19 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):

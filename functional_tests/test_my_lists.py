4c381a090d853fa6c94f574f922c41b20ce8535f
Evan Davis <me@ezdwt.com>
2017-01-27 18:33:22 -0800

    new_list view tries to assign owner but can't

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -28,3 +28,34 @@ class MyListsTest(FunctionalTest):
         self.create_pre_authenticated_session(email)
         self.browser.get(self.server_url)
         self.assert_logged_in(email)
+
+        # He goes to the home page and starts a list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Reticulate splines\n')
+        self.get_item_input_box().send_keys('Immanentize eschaton\n')
+        first_list_url = self.browser.current_url
+
+        # He notices a "My lists" link, for the first time
+        self.browser.find_element_by_link_text("My lists").click()
+
+        # He sees that his list is in there, named according to its
+        # first list item
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+
+        # He decides to start another list, just to see
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Click cows\n')
+        second_list_url = self.browser.current_url
+
+        # Under "My lists", his new list appears
+        self.browser.find_element_by_link_text("My lists").click()
+        self.browser.find_element_by_link_text("Click cows").click()
+        self.assertEqual(self.browser.current_url, second_list_url)
+
+        # He logs out. The "My lists" option disappears
+        self.browser.find_element_by_link_text("Log out").click()
+        self.assertEqual(
+            self.browser.find_elements_by_link_text("My lists"),
+            []
+        )
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils.html import escape
 from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR, ExistingListItemForm, ItemForm
 from lists.models import Item, List
 from unittest import skip
+User = get_user_model()
 
 
 class HomePageTest(TestCase):
@@ -134,9 +136,22 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        user = User.objects.create(email='a@example.com')
+        self.client.force_login(user)
+        self.client.post('/lists/new', data={'text': 'new item'})
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, user)
+
 
 class MyListsTest(TestCase):
     def test_my_lists_url_renders_my_lists_template(self):
+        User.objects.create(email="a@example.com")
         response = self.client.get('/lists/users/a@example.com/')
         self.assertTemplateUsed(response, 'my_lists.html')
 
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email='wrong_owner@example.com')
+        correct_user = User.objects.create(email="a@example.com")
+        response = self.client.get('/lists/users/a@example.com/')
+        self.assertEqual(response.context['owner'], correct_user)
